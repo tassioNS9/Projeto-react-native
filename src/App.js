6b1f71c0e087b5535a8e4b25d7d6889ce08f84ef
Tassio Neves <tassions@hotmail.com>
2026-05-06 09:25:30 -0300

    feat: add icons

--- src/App.js
+++ src/App.js
@@ -1,48 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { useColorScheme, View, Text, Button } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { enableScreens } from 'react-native-screens';
-enableScreens();
-const Stack = createNativeStackNavigator();
+import React from 'react';
+import Navigation from './navigation';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Details" component={DetailsScreen} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
-}
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title="Ir para detalhes" onPress={() => navigation.navigate('Details')} />
-    </View>
-  );
-}
-
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button title="Voltar" onPress={() => navigation.goBack()} />
-    </View>
-  );
+  return <Navigation />;
 }
 
 export default App;
