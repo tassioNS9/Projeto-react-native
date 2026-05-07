94f04e08b496a4136036678fdb2f21b4475e6ab4
Tassio Neves <tassions@hotmail.com>
2026-05-07 20:39:46 -0300

    feat: update navigation to use Edit screen

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { enableScreens } from 'react-native-screens';
 import HomeScreen from '../screens/Home';
-import DetailsScreen from '../screens/Details';
+import Edit from '../screens/Edit';
 enableScreens();
 
 const Stack = createNativeStackNavigator();
@@ -13,8 +13,8 @@ function Navigation() {
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Details" component={DetailsScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Inicio" component={HomeScreen} />
+          <Stack.Screen name="Edit" component={Edit} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
